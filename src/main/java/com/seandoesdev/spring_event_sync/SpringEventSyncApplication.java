a8076f3de76a6495439df84489fb2ce23721b6da
seandoesdev <seandoesdev@gmail.com>
2025-08-28 00:15:51 +0900

    first commit

---
+++ src/main/java/com/seandoesdev/spring_event_sync/SpringEventSyncApplication.java
@@ -0,0 +1,13 @@
+package com.seandoesdev.spring_event_sync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEventSyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEventSyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seandoesdev/spring_event_sync/SpringEventSyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seandoesdev.spring_event_sync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEventSyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
